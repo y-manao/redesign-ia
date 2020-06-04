7ec502d6d4ff43bb0f5455228e45e7b6974923b5
SUGIMOTO Tatsuo <sugi@zero-out.com>
2020-06-05 08:54:34 +0900

    Create index.js

---
+++ docs/js/index.js
@@ -0,0 +1,36 @@
+const uri = 'https://script.google.com/macros/s/AKfycbxyacpN8y4nxSAnU0Eji6E_rBRDFTY7YoWWFa0clY5ELRhskgpt/exec';
+const id = '1BpGnuwC4lZf9G2yFyiSrxbJuGO8gviV8mr-I2D3x4vA';
+const sheet = 'studio';
+const endpoint = `${uri}?id=${id}&sheet=${sheet}`;
+
+const renderJson = (json) => {
+  const studios = json.records;
+  studios.forEach(studio => {
+   const studioDiv = document.createElement('div');
+   const studioTitle = document.createElement("span");
+   studioTitle.className = 'studio-title';
+   studioTitle.textContent = studio['name-ja'];
+   const studioTitleEn = document.createElement("span");
+   studioTitleEn.className = 'studio-title-en';
+   studioTitleEn.textContent = studio['name-en'];
+   studioDiv.appendChild(studioTitle);
+   studioDiv.appendChild(studioTitleEn);
+   document.getElementById('studios').appendChild(studioDiv);
+ });
+  document.getElementById('result').textContent = JSON.stringify(json, null, 2);
+}
+
+const getData = async () => {
+  try{
+    const response =  await fetch(endpoint);
+    if(response.ok){
+      let jsonResponse = await response.json();
+            renderJson(jsonResponse);
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+getData();
